5c5d576c78fb25e7313a947db8cd0701b34034d1
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-18 10:32:10 +0530

    request

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView'
+import {elements} from './views/base'
+
+// Global state of the app
+// Search object
+// current recipe object
+// shopping list object
+// liked recipes
+
+const state = {
+
+};
+
+const controlSearch= async()=>{
+    // 1) Get query from view
+    const query = searchView.getInput();//TODO
+
+    if(query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "0a499c7982edde6fe0a96774285087fa";
+
+    try {
+      const res = await axios(
+        `${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
