77b4584ad0b2bdf52c01414b212ded079a5dbb95
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-04-19 20:06:25 +0530

    search

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView'
-import {elements} from './views/base'
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements,renderLoader,clearLoader } from "./views/base";
 
 // Global state of the app
 // Search object
@@ -8,29 +8,31 @@ import {elements} from './views/base'
 // shopping list object
 // liked recipes
 
-const state = {
+const state = {};
 
-};
-
-const controlSearch= async()=>{
+const controlSearch = async () => {
     // 1) Get query from view
-    const query = searchView.getInput();//TODO
+    const query = searchView.getInput(); //TODO
 
-    if(query){
+    if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResult();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes
         await state.search.getResults();
 
+        clearLoader();
         // 5) render results on UI
         searchView.renderResults(state.search.result);
     }
 };
 
-elements.searchForm.addEventListener('submit',e=>{
+elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,23 +2,37 @@ import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () =>{
+    elements.searchInput.value = '';
+};
+
+export const clearResult = ()=>{
+    elements.searchResList.innerHTML = '';
+};
+
+const limitRecipeTitle = (title, limit=19) => title.length > limit ? title.substring(0, title.substring(0, limit).lastIndexOf(' ')) + '...': title;
+
+
 const renderRecipe = recipe => {
-  const markup = `
+    const markup = `
         <li>
-            <a class="results__link results__link" href="#${recipe.recipe_id}">
+            <a class="results__link" href="#${recipe.recipe_id}" title="${recipe.title}">
                 <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
-                    <p class="results__author">${recipe.publiser}</p>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
-    elements.searchResList.insertAdjacentHTML('beforeend',markup);
+    elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page=1,resPerPage=10) => {
+    const start = 0;
+    const end = 10;
+
+    recipes.slice(start,end).forEach(renderRecipe);
 };
